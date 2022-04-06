9545928aeb8842f456c9e012454906d87e6a03b9
Yosua Nicolaus <cryodawn257@gmail.com>
2022-04-06 15:28:21 +0700

    fetch weather api and convert data

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("hello world");
+// let searchLocation = "Surabaya";
+const apiKey = "9c90876792bd2297343d7cf096085561";
+
+async function getWeather(location) {
+  const apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}&units=metric`;
+
+  let response = await fetch(apiURL);
+  let weather = await response.json();
+
+  data = convertData(weather);
+  console.log(data);
+}
+
+function convertData(weatherJson) {
+  const name = weatherJson.name;
+  const country = weatherJson.sys.country;
+  const status = weatherJson.weather[0].main;
+  const description = weatherJson.weather[0].description;
+  const temp = weatherJson.main.temp;
+  const feelsLike = weatherJson.main.feels_like;
+  const pressure = weatherJson.main.pressure;
+  const humidity = weatherJson.main.humidity;
+  const windSpeed = weatherJson.wind.speed;
+
+  return {
+    name,
+    country,
+    status,
+    description,
+    temp,
+    feelsLike,
+    pressure,
+    humidity,
+    windSpeed,
+  };
+}
